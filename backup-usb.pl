4b4fa8d48aa66c29478633aadfc502fa3084c166
Jon Vaughan <jon-git@turnip.org.uk>
2009-01-10 21:59:32 +0000

    more coding

--- backup-usb.pl
+++ backup-usb.pl
@@ -4,8 +4,9 @@ use strict;
 my $MAIN_POOL =		"tank";
 my $GPG_CMD =		"/opt/csw/bin/gpg";
 my $SNAPSHOT_PREFIX =	"backup-usb";
+my $BACKUP_PATH	=	"/backup-usb-1/filerbak"
 
-my @EXClUDE = qw| tank/swap tank/iscsi tank/tmp |;
+my @EXCLUDE = qw| tank/swap tank/iscsi tank/tmp |;
 
 my @filesystems = `zfs list -o name -H -r tank`;
 
@@ -14,18 +15,19 @@ foreach (@EXCLUDE) {
 }
 
 FS: foreach my $fs (@filesystems) {
-	my $prev_incr;
+	my $prev_incr = "";
 	my $new_incr;
 	my @snapshots = `zfs list -r -t snapshot $fs`;
 	@snapshots = sort grep { /^${fs}\@backup-usb/ } @snapshots;
 	
-	unless grep { /${SNAPSHOT_PREFIX}-full/ } @snapshots {
+	unless ( grep { /\@${SNAPSHOT_PREFIX}-full/ } @snapshots ) {
 		warn "no full backup!";
-		print "Would make full backup and delete any incrementals here!\n";
+		print "Would make full backup snapshot and delete any incrementals here!\n";
+		next FS;
 	}
 	
-	if ( my @incrs = grep { /${SNAPSHOT_PREFIX}-incr/ } @snapshots ) {
-		$incrs[$#incrs] =~ /${SNAPSHOT_PREFIX}-incr-(\d{4});
+	if ( my @incrs = sort grep { /\@${SNAPSHOT_PREFIX}-incr/ } @snapshots ) {
+		$incrs[$#incrs] =~ /\@${SNAPSHOT_PREFIX}-incr-(\d{4})/;
 		$prev_incr = $1;
 		$new_incr = $prev_incr + 1;
 		$new_incr = sprintf ("%05d", $new_incr);
@@ -34,10 +36,30 @@ FS: foreach my $fs (@filesystems) {
 		$new_incr = "0001"
 	}
 	
+	my $new_snapshot = "${SNAPSHOT_PREFIX}-incr-${new_incr}";
+	snapshot_and_send($fs, $new_snapshot, $prev_incr || 'full');
+	
+	
 	
 }
 
 sub snapshot_and_send {
-	my $ss_name = shift;
+	my $fs = shift;
+	my $snapshot = shift;
+	my $prev_incr = shift || 0;
+	
+	syscmd("zfs snapshot $fs\@$snapshot");
+	
+	my $zfs_send;
+	if ($prev_incr) {
+		$zfs_send = "zfs send -i $fs\@${SNAPSHOT_PREFIX}-incr-$prev_incr $fs\@$snapshot";
+		
+	}
+	else {
+		$zfs_send = "zfs send $fs\@snapshot";
+	}
+	
+	my $backup_file = "$fs\@$snapshot" =~ s|/|_|g;
+	$cmd = "$zfs_send | $GPG_CMD > $backup_path/$backup_file";
 	
 }
